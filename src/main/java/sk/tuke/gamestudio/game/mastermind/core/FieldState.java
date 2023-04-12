3bf391534ad93811da2363446e46be72886b68fe
Kyrylo Karunnyi <kyrylo.karunnyi@student.tuke.sk>
2023-04-12 02:14:33 +0200

    refresh project

--- src/main/java/sk/tuke/gamestudio/game/mastermind/core/FieldState.java
+++
@@ -1,5 +0,0 @@
-package sk.tuke.gamestudio.game.mastermind.core;
-
-public enum FieldState {
-    IN_PROGRESS, WON, LOST
-}
--- src/sk/tuke/kpi/kp/mastermind/core/FieldState.java
+++
@@ -1,5 +0,0 @@
-package sk.tuke.kpi.kp.mastermind.core;
-
-public enum FieldState {
-    IN_PROGRESS, WON, LOST
-}
